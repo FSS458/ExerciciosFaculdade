ef1ca6e2775921e34ae794905ee634434071c0d4
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2023-04-27 17:37:23 -0300

    Create exercicio5.c

---
+++ exercicio5.c
@@ -0,0 +1,14 @@
+// ex5
+    
+    int num1, sucessor, antecessor;
+    
+    printf("Digite o número que você deseja saber o sucessor e o antecessor: \n");
+    scanf("%d", &num1);
+    
+    sucessor = num1 + 1;
+    antecessor = num1 - 1;
+    
+    printf("O número que você digitou é: %d\n O sucessor deste número é: %d\n O antecessor deste número é: %d", num1, sucessor, antecessor);
+    
+    
+}
