548246299dcdc610821090952d7f01895dec5ddf
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2023-04-27 17:37:50 -0300

    Create ex7l2.c

---
+++ ex7l2.c
@@ -0,0 +1,21 @@
+// ex7
+# include <stdio.h>
+
+
+int main(){
+    
+    int num1, num2, subtracao;
+    
+    
+    printf("Digite aqui o primeiro número que você deseja subtrair: \n");
+    scanf("%d", &num1);
+    printf("Digite aqui o segundo número que você deseja subtrair: \n");
+    scanf("%d", &num2);
+    
+    subtracao = num1 - num2;
+    
+    printf("A subtração desses dois números é: %d\n", subtracao);
+    
+    
+    
+}
